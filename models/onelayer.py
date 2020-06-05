23e82606aec94eece7eff30e04e653abf9b34ec1
James Simon <james.simon@berkeley.edu>
2020-06-05 15:57:11 -0400

    tweaked OneLayer

--- models/onelayer.py
+++ models/onelayer.py
@@ -57,8 +57,9 @@ class OneLayerBase(nn.Module):
 
     def forward(self, x):
         x = x.view(x.size(0), -1)
-        # x = self.fc1(x)
-        return x[0:10]
+        x = self.fc1(x)
+        print(x.size())
+        return x
 
 
 class OneLayerCurve(nn.Module):
