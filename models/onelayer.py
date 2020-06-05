54771daecccc53e868e52b74ba1a580bff954482
James Simon <james.simon@berkeley.edu>
2020-06-05 10:49:49 -0400

    fixed OneLayer nomenclature

--- models/onelayer.py
+++ models/onelayer.py
@@ -12,7 +12,7 @@ INPUT_DIM = 512
 N_HIDDEN_NODES = 10
 DROPOUT_PROB = .5
 
-__all__ = ['OneLayer', 'OneLayerBN']
+__all__ = ['OneLayerReal', 'OneLayerBNActual']
 
 
 def make_layers(config, batch_norm=False, fix_points=None):
