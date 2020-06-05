7b4dc0b36ba9652e0a3645dc79903ebf83d291cd
James Simon <james.simon@berkeley.edu>
2020-06-05 16:41:23 -0400

    tweaked OneLayer

--- models/onelayer.py
+++ models/onelayer.py
@@ -60,7 +60,6 @@ class OneLayerBase(nn.Module):
 
     def forward(self, x):
         x = x.view(x.size(0), -1)
-        x = self.dropout1(x)
         x = self.fc1(x)
         print(x.size())
         return x
